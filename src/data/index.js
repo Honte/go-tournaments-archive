f21667a8aa6a3420d9608acff582fb753c8efa43
Artur Barcicki <barcicki@gmail.com>
2024-01-26 00:38:13 +0100

    Fixed time settings on 2 tournaments

--- src/data/index.js
+++ src/data/index.js
@@ -1,10 +1,13 @@
 import { loadTournaments } from './load'
 import { calculateStats } from '@/data/stats';
 
+const tournaments = await loadTournaments()
+const stats = calculateStats(tournaments);
+
 export function getTournaments() {
-  return loadTournaments()
+  return tournaments
 }
 
 export function getStats() {
-  return loadTournaments().then(calculateStats)
+  return stats
 }
