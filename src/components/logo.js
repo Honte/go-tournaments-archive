ddab38835d6349f21dc283ef19e442f69b60f4e9
Artur Barcicki <barcicki@gmail.com>
2024-09-21 16:32:07 +0200

    Made sure that logo keeps black stones on the bottom left

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,48 +1,50 @@
+const STROKE_WHEN_SAME = 1.587
+const STROKE_WHEN_DIFFERENT = 3.175
+
 export function Logo({...props}) {
   const color = props.color ?? 'black'
-  const oppositeColor = color === 'black' ? 'white' : 'black'
+  const isWhite = color === 'white' || color === '#fff' || color === '#ffffff'
 
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      color="#fff"
-      viewBox="0 0 66.801 134.31"
+      color="#000"
+      viewBox="0 0 66.801 135.101"
       {...props}
     >
       <path
         fill="currentColor"
-        d="M137.42 184.06c1.3-18.421 6.568-32.252 9.171-39.034-5.099 3.61-13.888 3.656-18.343 0 4.548 14.798 8.21 26.868 9.172 39.034zm12.465 27.693c-15.184-27.015-1.225-58.285 12.503-82.289 2.366-1.022 3.987-2.518 4.29-4.838-4.347 2.747-7.95 2.756-11.19 2.064.38 1.165 1.428 2.38 2.982 3.02-12.059 20.692-23.264 51.965-14.717 78.051h-12.667c8.548-26.086-2.658-57.358-14.717-78.05 1.554-.641 2.602-1.856 2.983-3.021-3.24.692-6.844.683-11.191-2.064.304 2.32 1.924 3.816 4.29 4.838 13.728 24.004 27.687 55.275 12.503 82.289z"
-        transform="translate(-103.224 -77.444)"
+        d="M33.404 107.409c1.3-18.421 6.568-32.252 9.171-39.034-5.099 3.61-13.888 3.656-18.343 0 4.548 14.798 8.21 26.868 9.172 39.034m12.465 27.693c-15.184-27.015-1.225-58.285 12.503-82.29 2.366-1.021 3.987-2.517 4.29-4.837-4.347 2.747-7.95 2.756-11.19 2.064.38 1.165 1.428 2.38 2.982 3.02-12.059 20.692-23.264 51.965-14.717 78.05H27.07c8.548-26.085-2.658-57.357-14.717-78.05 1.554-.64 2.602-1.855 2.983-3.02-3.24.692-6.844.683-11.19-2.064.303 2.32 1.923 3.816 4.29 4.838 13.727 24.004 27.686 55.275 12.502 82.289z"
       />
-      <g stroke="currentColor" transform="translate(-103.224 -77.444)">
+      <g stroke="currentColor" transform="translate(-104.016 -76.651)">
         <circle
           cx={137.42}
           cy={89.282}
           r={11.043}
-          fill="currentColor"
-          strokeWidth={1.587}
+          fill="white"
+          strokeWidth={isWhite ? STROKE_WHEN_SAME : STROKE_WHEN_DIFFERENT}
         />
         <circle
           cx={137.42}
           cy={131.51}
           r={11.043}
-          fill={oppositeColor}
-          strokeWidth={3.175}
+          fill={isWhite ? "black" : "currentColor"}
+          strokeWidth={isWhite ? STROKE_WHEN_DIFFERENT : STROKE_WHEN_SAME}
         />
         <circle
           cx={112.28}
           cy={-156.95}
           r={11.043}
-          fill="currentColor"
-          strokeWidth={1.587}
+          fill="white"
+          strokeWidth={isWhite ? STROKE_WHEN_SAME : STROKE_WHEN_DIFFERENT}
           transform="rotate(89.365)"
         />
         <circle
           cx={111.81}
           cy={-114.62}
           r={11.043}
-          fill={oppositeColor}
-          strokeWidth={3.175}
+          fill={isWhite ? "black" : "currentColor"}
+          strokeWidth={isWhite ? STROKE_WHEN_DIFFERENT : STROKE_WHEN_SAME}
           transform="rotate(89.365)"
         />
       </g>
