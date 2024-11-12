728f3207842df878c2d7c5e3fcf042894bc9c1ea
Artur Barcicki <barcicki@gmail.com>
2024-11-12 23:54:49 +0100

    Hid the banner

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -4,25 +4,15 @@ import { Medalists } from '@/components/medalists';
 import { Attendants } from '@/components/attendants';
 import { TotalStats } from '@/components/totalStats';
 import { loadTranslations } from '@/i18n/server';
-import { Banner } from '@/components/banner';
-import { getTranslator } from '@/i18n/translator';
 
 export default async function Home({ params: { locale } }) {
   const translations = await loadTranslations(locale);
   const tournaments = (await getTournaments()).toSorted((a, b) => b.id - a.id);
   const stats = await getStats();
 
-  const t = getTranslator(translations);
-
   return (
     <div className="xl:grid xl:grid-cols-4 xl:gap-4">
       <div className="xl:col-span-3 xl:row-span-5">
-        <Banner
-          href={t('currentEdition.website')}
-          tooltip={t('currentEdition.tooltip')}
-          title={t('currentEdition.title')}
-          subtitle={t('currentEdition.subtitle')}
-        />
         <Winners translations={translations} tournaments={tournaments}/>
       </div>
       <Medalists translations={translations} stats={stats}/>
