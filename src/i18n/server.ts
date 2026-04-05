f8c6968edb807a272c467e00e37e8d698c03bb33
Artur <barcicki@gmail.com>
2026-04-05 15:37:36 +0200

    Shared i18n

--- src/i18n/server.ts
+++ src/i18n/server.ts
@@ -1,9 +1,12 @@
 import EVENT from '@event';
 import fs from 'node:fs/promises';
+import { merge } from 'lodash-es';
 import { Locale, SUPPORTED_LOCALES, Translations } from '@/i18n/consts';
 
 export async function loadTranslations(locale: Locale): Promise<Translations> {
-  const translations = JSON.parse(await fs.readFile(`./events/${EVENT}/i18n/${locale}.json`, 'utf-8'));
+  const baseTranslations = JSON.parse(await fs.readFile(`./src/i18n/${locale}.json`, 'utf-8'));
+  const eventTranslations = JSON.parse(await fs.readFile(`./events/${EVENT}/i18n/${locale}.json`, 'utf-8'));
+  const translations = merge({}, baseTranslations, eventTranslations);
 
   translations.locale = locale;
 
