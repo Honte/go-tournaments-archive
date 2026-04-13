7001b6b5c6fcbb40dcc97851310727cfc8ca3ecc
Artur Barcicki <barcicki@gmail.com>
2026-04-13 21:48:06 +0200

    Added client-side redirect and loaders to pages

--- events/schema.ts
+++ events/schema.ts
@@ -1,7 +1,9 @@
+import type { Locale } from '@/i18n/consts';
+
 export type EventConfig = {
   id: string;
   domain: string;
-  defaultLocale: string;
+  defaultLocale: Locale;
   defaultCountry?: string;
   showCountry?: boolean;
   generatePngs?: boolean;
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function JustRedirect() {
-  return redirect(`/pl`);
-}
