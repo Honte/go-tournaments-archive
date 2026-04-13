e7a6bdb4c75df1653cb5494433397fbfb34f8dd5
Artur Barcicki <barcicki@gmail.com>
2026-04-13 23:54:17 +0200

    Fix year slider on events that have gaps

--- src/components/navigation/YearsNavigation.tsx
+++ src/components/navigation/YearsNavigation.tsx
@@ -2,43 +2,14 @@
 
 import clsx from 'clsx';
 import Link from 'next/link';
-import { type RefObject, useImperativeHandle, useState } from 'react';
-import { between } from '@/libs/math';
-
-export type YearsNavigationHandle = {
-  move: (direction: number) => void;
-  select: (year: number) => void;
-  current: () => number;
-};
 
 export type YearsNavigationProps = {
   locale: string;
   years: number[];
   current: number;
-  ref?: RefObject<YearsNavigationHandle | null>;
 };
 
-export function YearsNavigation({ locale, years, current, ref }: YearsNavigationProps) {
-  const min = years[0];
-  const max = years[years.length - 1];
-  const [selected, setSelected] = useState(between(min, current, max));
-
-  useImperativeHandle(
-    ref,
-    (): YearsNavigationHandle => ({
-      move(direction) {
-        setSelected(between(min, selected + Math.sign(direction), max));
-      },
-      select(year) {
-        setSelected(between(min, year, max));
-      },
-      current() {
-        return selected;
-      },
-    }),
-    [min, max, selected]
-  );
-
+export function YearsNavigation({ locale, years, current }: YearsNavigationProps) {
   // Whole years needs to be displayed on the sides because if they were removed from DOM while touched, touch events would stop firing
   // Instead, I'm hiding the ones which shouldn't be there
   return (
@@ -51,7 +22,7 @@ export function YearsNavigation({ locale, years, current, ref }: YearsNavigation
                 key={year}
                 href={`/${locale}/${year}`}
                 className={clsx('text-event-primary hover:text-event-hover font-bold underline', {
-                  hidden: year >= selected,
+                  hidden: year >= current,
                 })}
                 draggable={false}
                 prefetch={false}
@@ -60,14 +31,14 @@ export function YearsNavigation({ locale, years, current, ref }: YearsNavigation
               </Link>
             ))}
           </div>
-          <h1 className="text-4xl font-bold">{selected}</h1>
+          <h1 className="text-4xl font-bold">{current}</h1>
           <div className="absolute left-full flex gap-2 text-2xl">
             {years.map((year) => (
               <Link
                 key={year}
                 href={`/${locale}/${year}`}
                 className={clsx('text-event-primary hover:text-event-hover font-bold underline', {
-                  hidden: year <= selected,
+                  hidden: year <= current,
                 })}
                 draggable={false}
                 prefetch={false}
