6650ad1b09211f80b04550015cd48be994218f4a
Artur Barcicki <barcicki@gmail.com>
2026-04-18 17:51:52 +0200

    Added configuration for categories

--- events/pygc/config.ts
+++ events/pygc/config.ts
@@ -6,8 +6,17 @@ const EVENT_CONFIG: EventConfig = {
   defaultLocale: 'pl',
   showCountry: false,
   showBestPlace: false,
+  showCategories: true,
   hideGamesWithoutSgf: true,
   generateSvgs: true,
+  categories: [
+    'u21',
+    'u20',
+    'u18',
+    'u16',
+    'u15',
+    'u12'
+  ]
 };
 
 export default EVENT_CONFIG;
