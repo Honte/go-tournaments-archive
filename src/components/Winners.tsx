c33174e03a08a83b7d5770d296077640fd30fca6
Artur Barcicki <barcicki@gmail.com>
2026-04-18 22:44:11 +0200

    Added winners in home page

--- src/components/Winners.tsx
+++ src/components/Winners.tsx
@@ -1,61 +1,62 @@
+import EVENT_CONFIG from '@event/config';
 import type { Tournament } from '@/schema/data';
-import Link from 'next/link';
 import type { Translations } from '@/i18n/consts';
 import { getTranslator } from '@/i18n/translator';
-import { jsxJoin } from '@/libs/join';
+import { WinnersTable } from '@/components/WinnersTable';
 import { H1 } from '@/components/ui/H1';
-import { PlayerLink } from '@/components/ui/PlayerLink';
-import { PlayerName } from '@/components/ui/PlayerName';
+import { H2 } from '@/components/ui/H2';
 
-type WinnersProps = {
+export type WinnersProps = {
   tournaments: Tournament[];
   translations: Translations;
-  className?: string;
 };
 
-export function Winners({ tournaments, translations, className }: WinnersProps) {
+export function Winners({ tournaments, translations }: WinnersProps) {
   const t = getTranslator(translations);
 
   return (
-    <div className={className}>
+    <div>
       <H1>{t('winners.title')}</H1>
-      <table className="w-full border-collapse sm:table-fixed">
-        <thead className="border-b-gray-300 border-b">
-          <tr>
-            <th className="sm:w-24 md:w-36">{t('winners.year')}</th>
-            <th>{t('winners.first')}</th>
-            <th>{t('winners.second')}</th>
-            <th>{t('winners.third')}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tournaments.map(({ year, top, players }) => (
-            <tr key={year} className="text-center even:bg-gray-200 hover:bg-gray-300">
-              <td className="p-2">
-                <Link
-                  className="sm:text-xl font-bold text-event-primary underline hover:text-event-hover"
-                  href={`/${translations.locale}/${year}`}
-                  prefetch={false}
-                >
-                  {year}
-                </Link>
-              </td>
-              {top.map((winner, index) => (
-                <td className="p-1" key={index}>
-                  {jsxJoin(
-                    winner.split(',').map((id) => (
-                      <PlayerLink key={id} playerId={players[id].id} locale={translations.locale}>
-                        <PlayerName player={players[id]} />
-                      </PlayerLink>
-                    )),
-                    ', '
-                  )}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {EVENT_CONFIG.showCategories ? (
+        <CategoryWinners tournaments={tournaments} translations={translations} />
+      ) : (
+        <TotalWinners tournaments={tournaments} translations={translations} />
+      )}
     </div>
   );
+}
+
+function CategoryWinners({ tournaments, translations }: WinnersProps) {
+  const t = getTranslator(translations);
+
+  return (
+    <div className="flex flex-col gap-4">
+      {EVENT_CONFIG.categories?.map((category) => (
+        <div key={category}>
+          <H2>{t(`categories.full.${category}`)} </H2>
+          <WinnersTable results={getCategoryTop(tournaments, category)} translations={translations} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function TotalWinners({ tournaments, translations }: WinnersProps) {
+  return <WinnersTable translations={translations} results={tournaments} />;
+}
+
+function getCategoryTop(tournaments: Tournament[], category: string) {
+  const results = [];
+
+  for (const tournament of tournaments) {
+    if (tournament.categoriesTop?.[category]) {
+      results.push({
+        year: tournament.year,
+        top: tournament.categoriesTop[category],
+        players: tournament.players,
+      });
+    }
+  }
+
+  return results;
 }
