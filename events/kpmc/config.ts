8bf3de6265e75d9ad47a27b41481a21a9a130efa
Artur Barcicki <barcicki@gmail.com>
2026-04-19 00:01:51 +0200

    Added data for KPMC

---
+++ events/kpmc/config.ts
@@ -0,0 +1,12 @@
+import { EventConfig } from '@event/schema';
+
+const EVENT_CONFIG: EventConfig = {
+  id: 'wagc',
+  domain: '',
+  defaultLocale: 'en',
+  showCountry: true,
+  hideGamesWithoutSgf: true,
+  generateSvgs: true,
+};
+
+export default EVENT_CONFIG;
