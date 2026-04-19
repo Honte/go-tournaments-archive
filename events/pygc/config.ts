b5b4a63ca40a588afb674c705de3211796484762
Artur Barcicki <barcicki@gmail.com>
2026-04-19 10:08:06 +0200

    PYGC data improvements

--- events/pygc/config.ts
+++ events/pygc/config.ts
@@ -6,7 +6,6 @@ const EVENT_CONFIG: EventConfig = {
   defaultLocale: 'pl',
   showCountry: false,
   showBestPlace: false,
-  showCategories: true,
   hideGamesWithoutSgf: true,
   generateSvgs: true,
   categories: ['u21', 'u20', 'u18', 'u16', 'u15', 'u12'],
