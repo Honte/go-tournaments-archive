a37fc02748c0d156ece03aef47e6b9e3a7c095f4
Artur Barcicki <barcicki@gmail.com>
2026-05-03 16:56:52 +0200

    Attempt with unziping

--- public/index.php
+++ public/index.php
@@ -8,11 +8,14 @@ function select_best_language($languages) {
         if (isset($lq[1])) $lq[1]=floatval($lq[1]); else $lq[1]=$default_q--;
         $larr[$lq[0]]=$lq[1];
     }
-    usort($languages,function($a,$b) use ($larr) { return $larr[$b]<=>$larr[$a]; });
+    usort($languages, function($a, $b) use ($larr) {
+        if ($larr[$a] == $larr[$b]) return 0;
+        return ($larr[$a] < $larr[$b]) ? 1 : -1;
+    });
     return $languages[0];
 }
 
-$locale = select_best_language(['pl', 'en']);
+$locale = select_best_language(array('pl', 'en'));
 $server = $_SERVER['SERVER_NAME'];
 $port = isset($_SERVER['HTTP_X_FORWARDED_PORT']) && !empty($_SERVER['HTTP_X_FORWARDED_PORT']) ? ':'. $_SERVER['HTTP_X_FORWARDED_PORT'] : '';
 $protocol = $_SERVER['PROTOCOL'] = isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && !empty($_SERVER['HTTP_X_FORWARDED_PROTO']) ? "https://" : "http://";
