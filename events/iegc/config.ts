4cf132c7a7cdfe3c471cce100060316b677f375c
Artur Barcicki <barcicki@gmail.com>
2026-06-01 20:47:30 +0200

    Added IEGC event

---
+++ events/iegc/config.ts
@@ -0,0 +1,15 @@
+import { EventConfig } from '@event/schema';
+
+const EVENT_CONFIG: EventConfig = {
+  id: 'iegc',
+  domain: '',
+  locales: ['en'],
+  showCountry: false,
+  showBestPlace: true,
+  hideGamesWithoutSgf: true,
+  generateSvgs: true,
+  generateJpgs: true,
+  generateZips: true,
+};
+
+export default EVENT_CONFIG;
