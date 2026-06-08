f3471a06b252d13097e6b3f5a87b653d7b5070c8
Artur Barcicki <barcicki@gmail.com>
2026-06-08 18:55:18 +0200

    Added European Youth Go Championships Archive

---
+++ events/eygc/config.ts
@@ -0,0 +1,16 @@
+import { EventConfig } from '@event/schema';
+
+const EVENT_CONFIG: EventConfig = {
+  id: 'eygc',
+  domain: '',
+  locales: ['en'],
+  showCountry: true,
+  showBestPlace: true,
+  generateSvgs: false,
+  generateJpgs: true,
+  generateZips: true,
+  hideGamesWithoutSgf: true,
+  categories: ['u21', 'u20', 'u18', 'u16', 'u12'],
+};
+
+export default EVENT_CONFIG;
