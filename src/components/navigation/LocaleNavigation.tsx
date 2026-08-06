73fd2f65bb9028490fc298adf415b0d597374f72
Artur Barcicki <barcicki@gmail.com>
2026-08-07 01:44:33 +0200

    Search params aware locale navigation

--- src/components/navigation/LocaleNavigation.tsx
+++ src/components/navigation/LocaleNavigation.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname } from 'next/navigation';
 import { Link } from '@/components/navigation/Link';
+import { useNavigationSearchParams } from '@/hooks/useNavigation';
 
 export type LocaleNavigationProps = {
   locales: string[];
@@ -10,12 +11,14 @@ export type LocaleNavigationProps = {
 
 export function LocaleNavigation({ locale, locales }: LocaleNavigationProps) {
   const pathname = usePathname();
+  const searchParams = useNavigationSearchParams();
 
   if (locales.length <= 1 || !pathname) {
     return null;
   }
 
   const regex = new RegExp(`/${locale}(/|$)`);
+  const search = searchParams.toString();
 
   return (
     <div className="flex gap-3 text-sm text-event-light ml-auto">
@@ -23,7 +26,7 @@ export function LocaleNavigation({ locale, locales }: LocaleNavigationProps) {
         <Link
           key={nextLocale}
           className={nextLocale === locale ? 'font-bold' : 'underline'}
-          href={pathname.replace(regex, `/${nextLocale}$1`)}
+          href={`${pathname.replace(regex, `/${nextLocale}$1`)}${search ? `?${search}` : ''}`}
           aria-current={nextLocale === locale ? 'true' : undefined}
         >
           {nextLocale.toUpperCase()}
