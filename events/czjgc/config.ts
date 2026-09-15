78e6040b0544c28b8214ba93e422b858caa39d77
Artur Barcicki <barcicki@gmail.com>
2026-09-15 23:31:54 +0200

    Added Czech Junior Championships

---
+++ events/czjgc/config.ts
@@ -0,0 +1,13 @@
+import type { EventDefinition } from '@/schema/event';
+
+const EVENT_CONFIG: EventDefinition = {
+  id: 'czjgc',
+  locales: ['en'],
+  showCountry: false,
+  showBestPlace: true,
+  hideGamesWithoutSgf: true,
+  unknownRanks: ['30k'],
+  categories: ['u20', 'u18', 'u16', 'u15', 'u12'], 
+};
+
+export default EVENT_CONFIG;
